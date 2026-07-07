a6395dd27d1b8a55d9eaffa735ea41a9e2f86d6a
SouWasHere <103128108+SouWasHere@users.noreply.github.com>
2026-07-07 19:19:14 +0800

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,7 +4,7 @@ export const SITE = {
     rsvp: "https://forms.gle/VrLkur8aBtdcNdNs6",
     dressCode: "https://pin.it/QxASgCBFM",
     traditions: "#",
-    surprise: "/surprise",
+    surprise: "https://jodysavethedate.my.canva.site/advertisementttt",
     altoids: "#",
     upload: "https://drive.google.com/drive/folders/1hMI9bIVQIAlok2wregOeQOkuU6uDRSre?usp=sharing",
     giftGuide: "https://heyzine.com/flip-book/22788d63b2.html",
